ef89764751a43d77cd923e3839ae82f53349f6bc
Grzegorz Filipiak <g_filipiak@o2.pl>
2021-06-26 13:09:48 +0200

    testowy commit 2

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("Hello World");
         TestClass o1 = new TestClass("Jan","Kowalski");
         System.out.println(o1);
+        o1.changeName("Adam");
+        o1.changeLastName("Nowak");
+        System.out.println(o1);
     }
 }
--- src/pl/gda/wsb/TestClass.java
+++ src/pl/gda/wsb/TestClass.java
@@ -2,19 +2,42 @@ package pl.gda.wsb;
 
 public class TestClass {
 
-    String imie;
-    String nazwisko;
+    private String imie;
+    private String nazwisko;
 
     public TestClass(String imie, String nazwisko) {
         this.imie = imie;
         this.nazwisko = nazwisko;
     }
 
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void changeName(String newName){
+        setImie(newName);
+    }
+
+    public void changeLastName(String newLastName){
+        setNazwisko(newLastName);
+    }
     @Override
     public String toString() {
         return "TestClass{" +
-                "imie='" + imie + '\'' +
-                ", nazwisko='" + nazwisko + '\'' +
+                "imie='" + getImie() + '\'' +
+                ", nazwisko='" + getNazwisko() + '\'' +
                 '}';
     }
 }
